6cad3649dff1fafca42527c62e433a2de602acfb
tanvi kolte <tanvi.kolte@haptiq.com>
2025-07-16 00:25:38 +0530

    Week 1 Assignment

---
+++ Java/Week 1 - Assignment/StudentPerformance.java
@@ -0,0 +1,14 @@
+/* PROBLEM STATEMENT - 
+Design a command-line Java application that allows instructors to record and compute the academic 
+performance of multiple students across various subjects. 
+The app must allow entering marks, calculating averages, determining pass/fail status, 
+and saving the report to a file*/
+
+/* REQUIREMENTS - 
+Use Scanner for input
+Menu-driven system
+Input validation
+Calculate max/min/average grades
+Save report to text or JSON file*/
+
+
