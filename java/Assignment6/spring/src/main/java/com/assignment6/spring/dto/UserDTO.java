2df7be46d655d0cd73cbec29b624d26116738051
tanvi kolte <tanvi.kolte@haptiq.com>
2025-08-05 20:48:50 +0530

    Assignment 6 added

---
+++ java/Assignment6/spring/src/main/java/com/assignment6/spring/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.assignment6.spring.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private String name;
+    private String email;
+}
